a4eb3e1a548345dd1d836d3d1c0f8c34c6feedfd
artivis <jeremie.deray@canonical.com>
2023-08-23 18:07:35 +0200

    Allow setting any params
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- snap/local/launcher.sh
+++ snap/local/launcher.sh
@@ -3,6 +3,22 @@
 set -e
 
 MODEL="$(snapctl get model)"
-BAUDRATE="$(snapctl get baudrate)"
 
-${SNAP}/ros2 launch sllidar_ros2 sllidar_${MODEL}_launch.py serial_baudrate:=${BAUDRATE}
+LAUNCH_OPTIONS=""
+
+OPTIONS="baudrate channel-type serial-port serial-baudrate frame-id inverted \
+angle-compensate scan-mode udp-ip udp-port scan-frequency"
+
+for OPTION in ${OPTIONS}; do
+  VALUE="$(snapctl get ${OPTION})"
+  if [ -n "${VALUE}" ]; then
+    LAUNCH_OPTIONS+="${OPTION}:=${VALUE} "
+  fi
+done
+
+# Replace '-' with '_'
+LAUNCH_OPTIONS=$(echo ${LAUNCH_OPTIONS} | tr - _)
+
+logger -t ${SNAP_NAME} "Running with options: ${LAUNCH_OPTIONS}"
+
+${SNAP}/ros2 launch sllidar_ros2 sllidar_${MODEL}_launch.py ${LAUNCH_OPTIONS}
